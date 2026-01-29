e42afc84b02b79c166ab447d7e32bb959643891e
Linar502Prog <linarik502@gmail.com>
2026-01-30 00:00:50 +0400

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -2,9 +2,11 @@
 
 int main(){
     int arr[10]{1,2,3,4,5,6,7,8,9,10};
-    for(int i = 0;i<9;++i){
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0;i<size-1;++i){
         std::cout<<arr[i]<<',';
     }
-    std::cout<<arr[9]<<std::endl;
+    std::cout<<arr[size-1]<<std::endl;
     return EXIT_SUCCESS;
 }
+
