45514ff2fb080bc272e56ad1c4de877330ea6d11
Linar502Prog <linarik502@gmail.com>
2026-01-30 12:30:35 +0400

    Update Fourth.cpp

--- Fourth.cpp
+++ Fourth.cpp
@@ -4,27 +4,25 @@
 int main(){
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    int arr[10]{10,3,4,1,8,9,7,2,6,5};
     std::cout<<"Массив до сортировки: ";
-    for(int a = 0; a<10; ++a){
-        std::cout<<arr[a]<<" ";
+    int arr[10]{10, 3, 4, 1, 8, 9, 7, 2, 6, 5};
+    for(int i =0;i<10;i++){
+        std::cout<<arr[i]<<' ';
     }
     std::cout<<std::endl;
-    for(int i = 8; i>=0;--i){
-        for(int j = 9; j>0;--j ){
-            int rezerv=arr[i];
-            if(arr[i]<arr[j]){
-                arr[i]=arr[j];
-                arr[j]=rezerv;
+    for(int i = 0; i<9;i++){
+        for(int j =9; j>i ;j--){
+            int revers=arr[j];
+            if(arr[j]<arr[j-1]){
+                arr[j]=arr[j-1];
+                arr[j-1]=revers;
             }
-            
         }
     }
     std::cout<<"Массив после сортировки: ";
-    for(int b = 0; b<10; ++b){
-        std::cout<<arr[b]<<" ";
+    for(int i =0;i<10;i++){
+        std::cout<<arr[i]<<' ';
     }
-    std::cout<<std::endl;
-    return EXIT_SUCCESS:
-    
+    return EXIT_SUCCESS;
 }
+
